ce992fa653e5a4bcb9316e78537df1c20a1147a2
Carson Lauer <clauer23@brophybroncos.org>
2021-10-06 17:56:03 -0700

    Added a way to render images based on json data

--- moc-app/src/index.jsx
+++ moc-app/src/index.jsx
@@ -2,30 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import musicImage from "./imgs/Music/Template_Music_adobespark.jpeg"
-import activityImage from "./imgs/Activity/Template_Act_adobespark.png"
-import fdImage from "./imgs/F&D/Template_Food_adobespark.jpeg"
-
 
 class Button extends React.Component{
     render(){
         return(
             <div className="button">
-                <a><img src={this.props.src} alt={this.props.alt} /><h2>{this.props.text}</h2></a>
+                <a><img src={this.props.src} alt={this.props.text} /><h2>Gabbie's {this.props.text} Board</h2></a>
             </div>
             
         );
     }
 }
 class Main extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            error: null,
+            isLoaded: false,
+            data:[]
+        };
+    }
+    componentDidMount() {
+        fetch("./data.json")
+          .then(res => res.json())
+          .then(
+            (result) => {
+              this.setState({
+                isLoaded: true,
+                data: result
+              });
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error
+              });
+            }
+          )
+      }
+
     render(){
         return (
             <div>
                 <h1>Makers of Change 2021-2022</h1>
                 <div className="grid">
-                    <Button src={musicImage} alt="Music" text="Gabbie's Music Board"/>
-                    <Button src={activityImage} alt="Activity" text="Gabbie's Activity Board"/>
-                    <Button src={fdImage} alt="Food and Dance" text="Gabbie's Food and Dance Board"/>
+                    {this.state.data.map(item => {
+                        console.log(item);
+                        return <Button src={item.src} text={item.text} key={item.text}/>;
+                    })}
                 </div>
             </div>
         );
